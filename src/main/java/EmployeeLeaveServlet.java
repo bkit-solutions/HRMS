d3cec873af4688fb73eb5c329d1174cb2c34478d
BKIT_Solutions <baburao1000cr@gmail.com>
2025-11-20 12:09:49 +0530

    Update EmployeeLeaveServlet.java

--- src/main/java/EmployeeLeaveServlet.java
+++ src/main/java/EmployeeLeaveServlet.java
@@ -9,7 +9,7 @@ import org.json.JSONObject;
 
 @WebServlet("/EmployeeLeaveServlet")
 public class EmployeeLeaveServlet extends HttpServlet {
-    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/EmployeeManagementSystem";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/employeemanagementsystem";
     private static final String JDBC_USER = "==Mysql_Username==";
     private static final String JDBC_PASS = "==Mysql_Password==";
 
